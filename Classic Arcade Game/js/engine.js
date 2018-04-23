be5bcc42cf77d710f068243f736f533cc22fdf56
MaryB16 <buda.maria89@gmail.com>
2018-04-23 12:54:35 +0300

    Added gem images to the game

--- Classic Arcade Game/js/engine.js
+++ Classic Arcade Game/js/engine.js
@@ -208,6 +208,9 @@ var Engine = (function(global) {
         'images/char-horn-girl.png',
         'images/char-princess-girl.png',
         'images/char-pink-girl.png',
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png'
     ]);
     Resources.onReady(init);
 
